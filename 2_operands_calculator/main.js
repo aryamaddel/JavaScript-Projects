62c0d41a21cccdcd1a97eedf448a05473d3cd15d
Arya Maddel <maddelarya@gmail.com>
2023-05-06 09:09:19 +0530

    input box

--- 2_operands_calculator/main.js
+++ 2_operands_calculator/main.js
@@ -1,13 +1,24 @@
-let resultsEL = document.getElementById("results-el");
-function addition() {
-  resultsEL.innerText = 3 + 5;
-}
-function subtraction() {
-  resultsEL.innerText = 3 - 5;
-}
-function multiplication() {
-  resultsEL.innerText = 3 * 5;
-}
-function division() {
-  resultsEL.innerText = 3 / 5;
+const resultsEl = document.getElementById("results-el");
+
+function calculate(operator) {
+  const operand1 = Number(document.getElementById("operand1").value);
+  const operand2 = Number(document.getElementById("operand2").value);
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = operand1 + operand2;
+      break;
+    case "-":
+      result = operand1 - operand2;
+      break;
+    case "*":
+      result = operand1 * operand2;
+      break;
+    case "/":
+      result = operand1 / operand2;
+      break;
+  }
+
+  resultsEl.innerText = result;
 }
